0bff03d81b6afd9ea07fc4e0fbd4d514650c76fa
crotten <wildrunts@gmail.com>
2014-09-03 00:15:21 -0400

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 function MainController($scope) {
 
-  $scope.todo = "todos";
   $scope.list = [];
   $scope.slash = false;
+  $scope.edit = false;
 
   $scope.addTask = function (task) {
-    $scope.list.push(task);
+    $scope.list.push({
+      name:task.todo,
+      completed:false
+    });
     console.log(task);
     $scope.newTask = {};
   };
@@ -13,6 +16,14 @@ function MainController($scope) {
   $scope.removeTask = function (task) {
     $scope.list.splice($scope.list.indexOf(task), 1);
     console.log("Task is deleted!");
+  };
+
+  $scope.incompleteTodos = function () {
+    var total = 0;
+    $scope.list.forEach(function(task) {
+      total += task.completed === false;
+    });
+    return total;
   };
 
 }
